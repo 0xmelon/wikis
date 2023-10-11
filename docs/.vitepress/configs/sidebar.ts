1881ade3f6c7269eb9f92a4e75b927584f3fd247
Dreamin <you@example.com>
2023-10-11 09:36:52 +0800

    vault backup: 2023-10-11 09:36:51

--- docs/.vitepress/configs/sidebar.ts
+++ docs/.vitepress/configs/sidebar.ts
@@ -7,13 +7,10 @@ export const sidebar: DefaultTheme.Config['sidebar'] = {
     path.resolve(__dirname,"../../weekly/2023"),"2023年周报"
   ),
 
-  "/workflow/": sidebarAuto(
-    path.resolve(__dirname, "../../workflow/css"),
-
-  ).concat(sidebarAuto(
+  "/workflow/":sidebarAuto(
     path.resolve(__dirname, "../../workflow/computer-network"),"计算机网络"
     
-  )).concat(sidebarAuto(
+  ).concat(sidebarAuto(
     path.resolve(__dirname, "../../workflow/computer-focus"),"计算机组成原理"
     
   )).concat(sidebarAuto(
